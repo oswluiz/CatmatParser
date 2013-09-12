b8a3f391d02cea7a6e70f44906f7108a762bf287
Oswaldo S Ferreira <oswluizf@gmail.com>
2013-09-12 16:59:17 -0300

    New XML parser tests

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -1,22 +1,29 @@
 #coding: utf-8
 from os.path import abspath, dirname, join
+import sys
+
+ROOT_PATH = join(abspath(dirname(__file__)), '..')
+TEST_TEMPLATE = join(ROOT_PATH, 'tests', 'test_template.xml')
+sys.path.append(ROOT_PATH)
+
 from catmat.parser import CatmatParser
 from should_dsl import should, should_not
 import unittest
 
-ROOT_PATH = abspath(dirname(__file__))
-TEST_TEMPLATE = join(ROOT_PATH, 'test_template.xml')
-
 class TestCatmatParser(unittest.TestCase):
 
 	def setUp(self):
-		self.catmat_xml_parser = CatmatParser(TEST_TEMPLATE)
+		xml_string = open(TEST_TEMPLATE).read()
+		self.catmat_parser = CatmatParser(xml_string)
 
-	def test_parser_returns_list_of_hashes(self):
-		self.catmat_xml_parser.to_hash() |should| be_kind_of(list)
+	def test_parser_instance_should_accept_strings(self):
+		self.catmat_parser |should| be_kind_of (CatmatParser)
 
-	def test_parsing_response_cannot_be_empty(self):
-		self.catmat_xml_parser.to_hash() |should_not| be_empty
+	def test_parser_tohash_method_returns_something(self):
+		self.catmat_parser.to_hash() |should_not| be_empty
+
+	def test_parser_tohash_method_returns_a_list_of_hashes(self):
+		self.catmat_parser.to_hash() |should| be_kind_of (list)
 
 if __name__ == '__main__':
 	unittest.main()
