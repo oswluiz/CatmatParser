c9b80c35270389b945c6768ba31f98630469bf2e
Oswaldo S Ferreira <oswluizf@gmail.com>
2013-09-12 17:03:01 -0300

    stripping each text parse

--- catmat/parser.py
+++ catmat/parser.py
@@ -2,20 +2,19 @@ from xml.etree import ElementTree as ET
 
 class CatmatParser(object):
 
-    def __init__(self, template_url):
-        tree = ET.parse(template_url)
-        self.__root = tree.getroot()
+    def __init__(self, xml_string):
+        self.__tree = ET.fromstring(xml_string)
         self.__data = []
 
     def to_hash(self):
-        for item in self.__root.find('itens').findall('item'):
+        for item in self.__tree.find('itens').findall('item'):
             hash_data = {}
             unidades = []
-            descricao = item.find('descricao').text
+            descricao = item.find('descricao').text.strip()
             for unidade in item.find('unidades').findall('unidade'):
-                unidades.append([unidade.find('sigla_unidade_fornecimento').text,
-                                 unidade.find('capacidade').text,
-                                 unidade.find('unidade_medida').text])
+                unidades.append([unidade.find('sigla_unidade_fornecimento').text.strip(),
+                                 unidade.find('capacidade').text.strip(),
+                                 unidade.find('unidade_medida').text.strip()])
             hash_data['descricao'] = descricao
             hash_data['unidades']  = unidades
             self.__data.append(hash_data)
