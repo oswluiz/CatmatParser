50db929696a2c55e3e4931aaefffb1dc58c53c4b
Oswaldo S Ferreira <oswluizf@gmail.com>
2013-09-12 17:01:15 -0300

    Submitter now posts XML and use parser to parse output XML

--- catmat/submitter.py
+++ catmat/submitter.py
@@ -1,13 +1,15 @@
+# coding: utf-8
 from xml.etree import ElementTree as ET
-from splinter import Browser
+from catmat.parser import CatmatParser
 from os.path import join
-import urllib2
+import urllib, urllib2
+import requests
 
 class Submitter(object):
 
-    def __init__(self, cpf, password):
-    	url = "http://www.comprasnet.gov.br/XML"
-    	self.production_url = join(url, "producao/consultamatserv.asp")
+    def __init__(self, cpf=1, password=1):
+        url = "http://www.comprasnet.gov.br/XML"
+        self.production_url = join(url, "producao/consultamatserv.asp")
         self.development_url = join(url, "treinamento/consultamatserv.asp")
         self.__tree = ET.parse('catmat/input_template.xml')
         self.__cpf = cpf
@@ -15,30 +17,28 @@ class Submitter(object):
         self.__root = self.__tree.getroot()
         self.__data = []
 
-    def input_xml(self, radical1, radical2, radical3):
-    	root         = self.__root
-    	prefix_tag   = "{cnet_consultamatserv}"
-    	cpf_tag      = root.find('%scpf' %prefix_tag)
-    	password_tag = root.find('%ssenha' %prefix_tag)
-    	r1_tag       = root.find('%sradical1' %prefix_tag)
-    	r2_tag       = root.find('%sradical2' %prefix_tag)
-    	r3_tag       = root.find('%sradical3' %prefix_tag)
+    def _input_xml(self, radical1, radical2='', radical3=''):
+        xml_string = """<?xml version="1.0" encoding="ISO-8859-1"?>
+                            <cnet xmlns="cnet_consultamatserv">
+                            <ambiente>treinamento</ambiente>
+                            <sistema>XML</sistema>
+                            <cpf>%s</cpf>
+                            <senha>%s</senha>
+                            <acao>consulta material</acao>
+                            <codigo_item></codigo_item>
+                            <radical1>%s</radical1>
+                            <radical2>%s</radical2>
+                            <radical3>%s</radical3>
+                            <sustentavel></sustentavel>
+                        </cnet>""" %(self.__cpf, self.__password, radical1, radical2, radical3)
+        return xml_string
 
-    	# Writing Authentication params
-    	cpf_tag.text, password_tag.text = self.__cpf, self.__password
+    def post_xml(self, radical1, radical2='', radical3=''):
+        input_xml = self._input_xml(radical1, radical2, radical3)
+        param_data = {'xml': input_xml}
+        
+        output_xml = requests.post(self.development_url, data=param_data)
+        output_xml_string = output_xml.text.encode('utf-8')
+        parser = CatmatParser(output_xml_string)
 
-    	# Writing radicals
-    	r1_tag.text, r2_tag.text, r3_tag.text = radical1, radical2, radical3
-    	xml_string = ET.tostring(root, encoding="UTF-8")
-    	return xml_string
-
-	def post(self, url, data, contenttype):
-		request = urllib2.Request(url, data)
-		request.add_header('Content-Type', contenttype)
-		response = urllib2.urlopen(request)
-		return response.read()
-
-	def post_xml(radical1, radical2, radical3):
-		url = self.production_url
-		data = input_xml(radical1, radical2, radical3)
-		return post(url, data, "text/xml")
+        return parser.to_hash()
