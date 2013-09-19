a8fb3e64fa03c985cdb0190d051b3c3d482f1e5f
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2013-09-19 12:14:57 -0300

    Upgrade version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'A simple Catmat (SIASG) XML Parser',
+    'description': 'A simple Catmat (SIASG) XML Service',
     'author': 'Oswaldo Ferreira',
     'url': 'https://github.com/oswluiz/CatmatParser',
     'download_url': 'https://github.com/oswluiz/CatmatParser',
     'author_email': 'oswluizf@gmail.com',
-    'version': '1.0',
-    'install_request': ['should_dsl', 'requests'],
+    'version': '1.2',
+    'install_requires': ['should_dsl', 'requests'],
     'packages': ['catmat'],
     'scripts': [],
     'name': 'catmat-parser'
