60e2a582e508f0185cb97a28332e2b11ce0ee740
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2013-10-02 11:44:05 -0300

    Easing ambient environment variable setting on Submitter

--- catmat/submitter.py
+++ catmat/submitter.py
@@ -8,10 +8,11 @@ ROOT_PATH = join(abspath(dirname(__file__)))
 
 class Submitter(object):
 
-    def __init__(self, cpf=1, password=1):
+    def __init__(self, ambiente="treinamento", cpf=1, password=1):
         url = "http://www.comprasnet.gov.br/XML"
-        self.production_url = join(url, "producao/consultamatserv.asp")
-        self.development_url = join(url, "treinamento/consultamatserv.asp")
+        self.__urls = { 'produção'   : join(url, "producao/consultamatserv.asp"), 
+                        'treinamento': join(url, "treinamento/consultamatserv.asp") }
+        self.ambiente = ambiente
         self.__input_xml_path = join(ROOT_PATH, 'input_template.xml')
         tree = ET.parse(self.__input_xml_path)
         self.__cpf = cpf
@@ -21,17 +22,17 @@ class Submitter(object):
 
     def _get_xml_input_by_radical(self, radical1, radical2='', radical3=''):
         codigo = ''
-        return open(self.__input_xml_path, 'rw').read() %(self.__cpf, self.__password, 
+        return open(self.__input_xml_path, 'rw').read() %(self.ambiente, self.__cpf, self.__password,
                                                          codigo, radical1, radical2, radical3)
 
     def _get_xml_input_by_cod(self, codigo):
         radical1, radical2, radical3 = '', '', ''
-        return open(self.__input_xml_path, 'rw').read() %(self.__cpf, self.__password, 
+        return open(self.__input_xml_path, 'rw').read() %(self.ambiente, self.__cpf, self.__password,
                                                          codigo, radical1, radical2, radical3)
 
     def _post_data(self, input_xml):
         param_data = {'xml': input_xml}
-        output_xml = requests.post(self.development_url, data=param_data)
+        output_xml = requests.post(self.__urls[self.ambiente], data=param_data)
         output_xml_string = output_xml.text.encode('utf-8')
         return CatmatParser(output_xml_string)
 
