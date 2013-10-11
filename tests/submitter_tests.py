372549e6faf9ca7abcb3a7266667dc141682c3e5
Oswaldo S Ferreira <oswluizf@gmail.com>
2013-10-11 11:45:14 -0300

    Updating Treinamento ambience tests

--- tests/submitter_tests.py
+++ tests/submitter_tests.py
@@ -10,21 +10,39 @@ from should_dsl import should, should_not
 import unittest
 
 ROOT_PATH = abspath(dirname(__file__))
-TEST_TEMPLATE = join(ROOT_PATH, 'test_template.xml')
+TEST_TEMPLATE = join(ROOT_PATH, 'tests' , 'test_template.xml')
 
 class TestSubmitter(unittest.TestCase):
 
 	def setUp(self):
-		self.submitter = Submitter(cpf=123, password=321)
+		self.treinamento_submitter = Submitter()
 
-	def test_xml_inputter_returns_requires_at_least_one_arg(self):
-		self.submitter._input_xml('foo') |should_not| be_empty
+	def test_default_ambiente_must_be_treinamento(self):
+		self.treinamento_submitter.ambiente |should| equal_to('treinamento')
 
-	def test_xml_inputter_returns_a_string(self):
-		self.submitter._input_xml('FOO', 'BAR', 'SUP') |should| be_kind_of(str)
+	def test_populates_default_xml_with_submitter_radical_arguments(self):
+		self.treinamento_submitter._get_xml_input_by_radical('foo', 'bar', 'suap') |should| be_kind_of(str)
 
-	def test_post_xml_returns_a_list_of_hashes(self):
-		self.submitter.post_xml('lapis', 'preto') |should| be_kind_of(list)
+	def test_populates_default_xml_with_submitter_cod_arguments(self):
+		self.treinamento_submitter._get_xml_input_by_radical('123') |should| be_kind_of(str)
+
+	def test_gets_list_of_hashes_of_catmat_material_using_radical_params(self):
+		self.treinamento_submitter.get_by_radical('lapis', 'preto') |should| be_kind_of(list)
+
+	def test_gets_list_of_hashes_of_catmat_material_using_cod_param(self):
+		self.treinamento_submitter.get_by_cod('000391674') |should| be_kind_of(list)
+
+	def test_raises_value_error_if_no_material_is_found_using_radical_params(self):
+		try:
+			self.treinamento_submitter.get_by_radical('foo', 'bar') |should| be_kind_of(list)
+		except:
+			return True
+
+	def test_raises_value_error_if_no_material_is_found_using_cod_params(self):
+		try:
+			self.treinamento_submitter.get_by_cod('123') |should| be_kind_of(list)
+		except:
+			return True
 
 if __name__ == '__main__':
 	unittest.main()
